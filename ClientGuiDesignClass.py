ea9ef19e734c25ac386680682ec17b2b7528cee5
Jens <hjerpe82@gmail.com>
2017-10-22 22:09:11 +0200

    Fixed entrywidgets functions and design for GUI

--- ClientGuiDesignClass.py
+++ ClientGuiDesignClass.py
@@ -11,6 +11,8 @@ class ClientGui:
 		self.client_port = client_port_
 
 		self.root = tkinter.Tk()
+		self.root.title('GONE CHAT')
+		self.root.iconbitmap(default='images/ikon.ico')
 		self.root.configure(bg='white')
 		self.root.minsize(width=800, height=600)
 
@@ -24,13 +26,11 @@ class ClientGui:
 		self.root.rowconfigure(2, weight=1, minsize=200)
 
 		# base frames for GUI
-		self.top_left_frame = tkinter.Frame(self.root, bg='red', highlightbackground='blue',highlightthickness=2)
+		self.top_left_frame = tkinter.Frame(self.root, bg='white')
 		self.top_right_frame = tkinter.Frame(self.root)
-		self.center_left_frame = tkinter.Frame(self.root, bg='green', highlightbackground='blue',highlightthickness=2)
-		self.center_right_frame = tkinter.Frame(self.root, bg='yellow', highlightbackground='blue',highlightthickness=2)
-		self.bottom_frame = tkinter.Frame(self.root, bg='white', highlightbackground='blue',highlightthickness=2)
-
-		self.chat_window = tkinter.Text(self.center_left_frame, highlightbackground='blue',highlightthickness=2)
+		self.center_left_frame = tkinter.Frame(self.root, bg='white', relief='ridge', borderwidth=2)
+		self.center_right_frame = tkinter.Frame(self.root, bg='white', relief='ridge', borderwidth=2)
+		self.bottom_frame = tkinter.Frame(self.root, bg='white')
 
 		# variables for easy change of design
 		self.font = 'calibri',
@@ -39,6 +39,13 @@ class ClientGui:
 		self.background_color = 'white'
 		self.frame_color = 'green'
 
+		# Text widgets created
+		self.chat_window = tkinter.Text(self.center_left_frame, wrap=tkinter.WORD, font=(self.font, 12))
+		self.send_message_frame = tkinter.Frame(self.bottom_frame)
+		self.input_window = tkinter.Text(self.send_message_frame, width=50, height=3, borderwidth=2, wrap=tkinter.WORD, relief='ridge', font=(self.font, 12))
+
+
+
 		self.list_of_active_users = ['Kim', 'Nabil','Peter']
 
 	def start(self):
@@ -46,7 +53,7 @@ class ClientGui:
 		self.top_left_frame_create()
 		self.top_right_frame_create()
 		self.center_left_frame_create()
-		self.center_right_frame_create(self.list_of_active_users)
+		self.center_right_frame_create()
 		self.bottom_frame_create()
 		self.root.mainloop()
 
@@ -66,21 +73,21 @@ class ClientGui:
 
 
 	def top_left_frame_create(self):
-		logo_image = tkinter.PhotoImage(file='G.ONE.png')
-		logo_label = tkinter.Label(self.top_left_frame, image=logo_image, border=0)
+		logo_image = tkinter.PhotoImage(file='images/Gone_logo.png')
+		logo_label = tkinter.Label(self.top_left_frame, image=logo_image, border=0, bg='white')
 		logo_label.image = logo_image
 		logo_label.grid(row=0, column=0, pady=15, sticky='w')
 
 	def top_right_frame_create(self):
-		user_info_frame = tkinter.Frame(self.top_right_frame, bg = 'darkblue', width= 20, height = 20, pady=10, highlightbackground='white',highlightthickness=1)
+		user_info_frame = tkinter.Frame(self.top_right_frame, bg = '#3d85c6', width= 20, height = 20, pady=10, relief="sunken", borderwidth=1)
 		user_info_frame.grid(row=0, column=0, ipadx=10)
 
-		user_label = tkinter.Label(user_info_frame, text='Username: ', justify='left')
-		username_label = tkinter.Label(user_info_frame, text=self.user_name)
-		ip_label = tkinter.Label(user_info_frame, text='User IP: ', justify='left')
-		user_ip_label = tkinter.Label(user_info_frame, text=self.client_ip)
-		port_label = tkinter.Label(user_info_frame, text='User Port: ', justify='left')
-		user_port_label = tkinter.Label(user_info_frame, text=self.client_port, anchor='w')
+		user_label = tkinter.Label(user_info_frame, text='Username: ', justify='left', bg = '#3d85c6', fg='white', font=(self.font, 12))
+		username_label = tkinter.Label(user_info_frame, text=self.user_name, bg = '#3d85c6', fg='white', font=(self.font, 12))
+		ip_label = tkinter.Label(user_info_frame, text='User IP: ', justify='left', bg = '#3d85c6', fg='white', font=(self.font, 12))
+		user_ip_label = tkinter.Label(user_info_frame, text=self.client_ip, bg = '#3d85c6', fg='white', font=(self.font, 12))
+		port_label = tkinter.Label(user_info_frame, text='User Port: ', justify='left', bg = '#3d85c6', fg='white', font=(self.font, 12))
+		user_port_label = tkinter.Label(user_info_frame, text=self.client_port, anchor='w', bg = '#3d85c6', fg='white', font=(self.font, 12))
 
 		user_label.grid(row=0, column=0, sticky='w', padx=10)
 		username_label.grid(row=0, column=1, sticky='w')
@@ -91,9 +98,14 @@ class ClientGui:
 		port_label.grid(row=2, column=0, sticky='w', padx=10)
 		user_port_label.grid(row=2, column=1, sticky='w')
 
-		change_userinfo_button = tkinter.Button(user_info_frame, text='Properties')
-		change_userinfo_button.grid(row=3, column=0, pady=(10, 0))
-		log_out_button = tkinter.Button(user_info_frame, text='Log out')
+		properties_image = tkinter.PhotoImage(file='images/button_properties.png')
+		change_userinfo_button = tkinter.Button(user_info_frame, image=properties_image, borderwidth=0, background='#3d85c6', activebackground='#3d85c6')
+		change_userinfo_button.image = properties_image
+		change_userinfo_button.grid(row=3, column=0, pady=(10, 0), padx=10)
+
+		log_out_image=tkinter.PhotoImage(file='images/button_log-out.png')
+		log_out_button = tkinter.Button(user_info_frame, image=log_out_image, borderwidth=0, background='#3d85c6', activebackground='#3d85c6')
+		log_out_button.image = log_out_image
 		log_out_button.grid(row=3, column=1, pady=(10, 0))
 
 	def center_left_frame_create(self):
@@ -110,13 +122,18 @@ class ClientGui:
 		chat_window_scrollbar.config(command=self.chat_window.yview)
 		'''
 
-	def center_right_frame_create(self, list_of_active_users):
+	def center_right_frame_create(self):
+		def send_private_message(event):
+			chosen_user = self.list_of_active_users[active_user_list.curselection()[0]]
+			self.input_window.delete('1.0', 'end')
+			self.input_window.insert('1.0', '@{} '.format(chosen_user))
 
-		active_user_list = tkinter.Listbox(self.center_right_frame)
+		active_user_list = tkinter.Listbox(self.center_right_frame, font=(self.font, 12))
 		active_user_list.grid(row=0, column=0, sticky='nswe')
 
+		active_user_list.bind('<Button-3>', send_private_message)
 
-		for user in list_of_active_users:
+		for user in self.list_of_active_users:
 			active_user_list.insert('end', user)
 
 
@@ -150,15 +167,27 @@ class ClientGui:
 		input_field.grid(row=1, column=0, sticky='ew')
 		'''
 
+
 		def get_message_from_input_window():
-				message = input_window.get("1.0", 'end-1c')
+			message = self.input_window.get("1.0", 'end-1c')
+			if message.isspace() or len(message) == 0:
+				self.input_window.delete('1.0', 'end')
+			else:
 				ClientGuiFunctions.print_message_in_text_frame(message, self.chat_window)
+				self.input_window.delete('1.0', 'end')
 
-		send_message_frame=tkinter.Frame(self.bottom_frame, highlightbackground='blue', highlightthickness=2)
-		send_message_frame.grid(row=0, column=0)
+		def get_message_from_input_window_with_enter(event):
+			get_message_from_input_window()
+			return 'break'
 
-		input_window = tkinter.Text(send_message_frame, highlightbackground='blue', highlightthickness=2,  width=50, height=3)
-		input_window.grid(row=0, column=0, sticky='nswe')
+		def new_line(event):
+			self.input_window.insert('insert', '{}'.format(''))
+
+		self.send_message_frame.grid(row=0, column=0)
+		self.input_window.bind('<Return>', get_message_from_input_window_with_enter)
+		self.input_window.bind('<Shift-Return>', new_line)
+
+		self.input_window.grid(row=0, column=0, sticky='nswe')
 
 		'''
 		Scrollbar for send_widget. Saved for possible later implemention. If not implemented before release, delete this
@@ -168,11 +197,12 @@ class ClientGui:
 		input_window.configure(yscrollcommand=message_send_scrollbar.set)
 		message_send_scrollbar.config(command=input_window.yview)
 		'''
-
-		send_button = tkinter.Button(self.bottom_frame, text='Send', command=get_message_from_input_window)
+		send_image = tkinter.PhotoImage(file='images/button_send.png')
+		send_button = tkinter.Button(self.bottom_frame, image=send_image, command=get_message_from_input_window, borderwidth=0)
+		send_button.image = send_image
 		send_button.grid(row=1, column=0, sticky='e', padx=10, pady=10)
 
-
+# TODO - Dropdown for activeclientlist
 
 
 test = ClientGui('16', '16', 'Kim', '127.0.0.1', '5555')
