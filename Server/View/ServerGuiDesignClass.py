d9e33f37479b9afa1275973538e95c0d917c6d36
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-10-23 20:20:12 +0200

    Chat box and entry box both added and now functioning with send button. Various colours added for fun.

--- Server/View/ServerGuiDesignClass.py
+++ Server/View/ServerGuiDesignClass.py
@@ -15,46 +15,43 @@ class ServerGui:
 
         self.root = tkinter.Tk()
 
-        self.root.title('GONE CHAT')
+        self.root.title('GONE CHAT - SERVER')
         self.root.iconbitmap(default='../../images/ikon.ico')
-        self.root.configure(bg='white')
+        self.root.configure(bg='ivory2')
         # width x height + x offset + y offset:
         self.root.geometry("1200x700+30+30")
         self.root.minsize(width = 1200, height = 700)
 
-
-
-#################################################################3
-
         #Gui framwork
         self.logo_frame = tkinter.Frame(self.root, highlightbackground = 'black', highlightthickness= 2)
-        self.chat_frame = tkinter.Frame(self.root, bg='white', highlightbackground = 'black', highlightthickness= 2)
-        self.chat_entry_frame = tkinter.Frame(self.root, bg='white', highlightbackground = 'black', highlightthickness= 2)
-        self.send_button = tkinter.Frame(self.root, bg='white', highlightbackground='black', highlightthickness=2)
-        self.server_info = tkinter.Frame(self.root, bg='white', highlightbackground='black', highlightthickness=2)
-        self.server_list = tkinter.Frame(self.root, bg='white', highlightbackground='black', highlightthickness=2)
+        self.chat_window = tkinter.Frame(self.root, bg='wheat3', highlightbackground = 'black', highlightthickness= 2)
+        self.chat_entry_frame = tkinter.Frame(self.root, bg='bisque2', highlightbackground = 'black', highlightthickness= 2)
+        self.send_button = tkinter.Frame(self.root, bg='ivory2', highlightbackground='black', highlightthickness=2)
+        self.server_info = tkinter.Frame(self.root, bg='gray18', highlightbackground='black', highlightthickness=2)
+        self.server_list = tkinter.Frame(self.root, bg='bisque2', highlightbackground='black', highlightthickness=2)
 
-        self.input_window = tkinter.Text(self.chat_entry_frame, width=50, height=3, borderwidth=2, wrap=tkinter.WORD,relief='ridge')
 
 
         self.logo_frame.place(x = 20, y = 30, width = 300, height = 70)
-        self.chat_frame.place(x = 20, y = 110, width = 700, height = 400)
-        self.chat_entry_frame.place(x = 20, y = 530, width = 600, height = 90)
+        self.chat_window.place(x = 20, y = 110, width = 700, height = 400)
+        #self.chat_entry_frame.place(x = 20, y = 530, width = 600, height = 90)
         #self.send_button.place(x = 630, y = 530, width = 90, height = 90)
         self.server_info.place(x = 630, y = 30, width = 300, height = 70)
         self.server_list.place(x = 730, y = 110, width = 200, height = 510)
 
+        # Text widgets created
+        self.chat_window = tkinter.Text(self.chat_window, wrap=tkinter.WORD)
+        self.send_message_frame = tkinter.Frame(self.chat_entry_frame)
+        self.input_window = tkinter.Text(self.root, width=50, height=3, borderwidth=2, wrap=tkinter.WORD)
 
 
     def start(self):
-        #Function to start
+
         self.logo_create()
         self.chat_input_frame()
-        self.chat_button_create()
+        self.chat_frame_create()
 
         self.root.mainloop()
-
-
 
 
     def logo_create(self):
@@ -63,34 +60,48 @@ class ServerGui:
         logo_label.image = logo_image
         logo_label.place(x = 20, y = 30, width = 300, height = 70)
 
-    def chat_button_create(self):
-        self.send_image = tkinter.PhotoImage(file='../../images/button_send.png')
-        self.send_button = tkinter.Button(self.root, image=self.send_image, command = lambda: self.test_print()) #Glöm ej ändra funktion
-        self.send_button.image = self.send_image
-        self.send_button.place(x=630, y=530, width=90, height=90)
 
 
 
     def chat_frame_create(self):
-        self.chat_frame.place(x=20, y=110, width=700, height=400)
-        self.chat_frame.see('end')
-        self.chat_frame.configure(state="disabled")
+        self.chat_window.place(x = 0, y = 0, width = 695, height = 395)
+        self.chat_window.see('end')
+        self.chat_window.configure(state="disabled")
+
+
 
     def chat_input_frame(self):
+
         def get_message_from_input_window():
-            message = self.chat_input_frame().get("1.0", 'end-1c')
+            message = self.input_window.get("1.0", 'end-1c')
             if message.isspace() or len(message) == 0:
-                self.chat_input_frame().delete('1.0', 'end')
+                self.input_window.delete('1.0', 'end')
             else:
-                ServerGuiFunctions.print_message_in_text_frame(message, self.chat_frame_create())
+                ServerGuiFunctions.print_message_in_text_frame(message, self.chat_window)
                 self.input_window.delete('1.0', 'end')
 
+        def send_with_enter(event):
+            get_message_from_input_window()
+            return 'break'
+
+        def new_line(event):
+            self.input_window.insert('insert', '{}'.format(''))
+
+        self.chat_entry_frame.place(x=20, y=530, width=600, height=90)
+        self.input_window.bind('<Return>', send_with_enter)
+        self.input_window.bind('<Shift-Return>', new_line)
+
+        self.input_window.place(x=20, y=530, width=600, height=90)
+
+        send_image = tkinter.PhotoImage(file='../../images/button_send.png')
+        send_button = tkinter.Button(self.root, image=send_image, command = get_message_from_input_window) #Glöm ej ändra funktion
+        send_button.image = send_image
+        send_button.place(x=630, y=530, width=90, height=90)
 
 #############
     def test_print(self):
         print("Hello, world")
 #############
-
 
 
 
